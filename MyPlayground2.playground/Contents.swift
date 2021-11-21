22f264ec50c4a729efe14f8ad07654704dcf0316
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-11-21 16:19:18 +0300

    second

---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+var dictionary = [
+    "Kobe": 8,
+    "Lebron": 23,
+    "Rondo": 9
+]
+
+dictionary["Fisher"] = 2
+
+dictionary["Kobe"] = 24
+
+for i in dictionary{
+    print(i)
+}
+
--- MyPlayground_test.playground/Contents.swift
+++ MyPlayground_test.playground/Contents.swift
@@ -1,30 +1,36 @@
 import UIKit
 
-for i in 1...255{
+
+
+
+var dictionary = [
+    "Kobe": 8,
+    "Lebron": 23,
+    "Rondo": 9
+]
+
+dictionary["Fisher"] = 2
+
+dictionary["Kobe"] = 24
+
+for i in dictionary{
     print(i)
 }
 
-for i in 1...100{
-    if(i%3==0 || i%5==0) {
-       print(i)
-    }
+var myInt = 1
+func changeMyInt(someInt: Int) {
+    someInt = someInt + 1           // This throws an error because we are trying to modify a constant.
+    print(someInt)
 }
 
 
-for i in 1...100{
-    if(i%3==0) {
-        print("Fizz")
-    }
-    else if(i%5==0){
-        print("Buzz")
-    }
-    else if(i%3==0 && i%5==0 ){
-        print("FizzBuzz")
-    }
-    else{
-        print(i)
-    }
-}
+
+
+
+
+
+
+
 
 
 
